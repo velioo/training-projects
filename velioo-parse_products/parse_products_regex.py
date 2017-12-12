d9c74bff188cd062fa7207e8c712019d6bf24d9a
velioo <velioocs@mail.bg>
2017-12-12 13:57:14 +0200

    Changing regex html parser

--- velioo-parse_products/parse_products_regex.py
+++ velioo-parse_products/parse_products_regex.py
@@ -1,18 +1,22 @@
 from urllib.request import urlopen
 from urllib.request import urlretrieve
+import urllib.error
 import re
 import validators
 import sys
-import pymysql.cursors
+import os
+import pymysql
 from random import randint
 from html.parser import HTMLParser
 import os
 import shutil
 import requests
+from socket import timeout
 
 html_p = HTMLParser()
 base_url = sys.argv[1] if (len(sys.argv) > 1 and sys.argv[1]) else False
 visited = []
+to_visit = []
 products_inserted = 0
 
 
@@ -33,9 +37,23 @@ def main():
 				visit_url(base_url, connection)
 			else:
 				print("URL is not valid")
-			
+
+		except pymysql.err.OperationalError as oe:
+			print(oe)
+		except pymysql.err.InternalError as ie:
+			print(ie)
+		except pymysql.err.DatabaseError as de:
+			print(de)
+		except pymysql.err.InterfaceError as ie:
+			print(ie)
+		except pymysql.err.Error as e:
+			print(e)	
+		except AttributeError as ae:
+			print(ae)
+		except TypeError as te:
+			print(ty)
 		except Exception as e:
-			print(e)
+			print('Unknown error occured',e)
 		finally:
 			connection.close()	
 			
@@ -48,10 +66,11 @@ def visit_url(url, connection):
 		try:
 			print("Visiting: " + url)
 			visited.append(url)
-			response = urlopen(url)
-			if(response.headers.get_content_charset() == None):
-				return
-			lines = response.read().decode(response.headers.get_content_charset())
+			response = urlopen(url, timeout = 10)
+			if response.info().get_content_type() != 'text/html':
+				print('Url', url , ' is not text/html, returning...')
+				return		
+			lines = response.read().decode('utf-8')
 			matched = re.findall(r'<input\s*type\s*=\s*"\s*hidden\s*"\s*class\s*=\s*"\s*soaring-cart-data\s*"\s*data-url\s*=\s*"\s*(.+)\s*"\s*data-img_url\s*=\s*"(.+)\s*"\s*data-name\s*=\s*"\s*(.+)\s*"\s*data-price\s*=\s*"\s*(\d+)\s*"', lines)	
 			for data in matched:
 				if len(data) == 4:
@@ -60,7 +79,11 @@ def visit_url(url, connection):
 					product_image_url = html_p.unescape(html_p.unescape(product_image_url.replace("96x96", "200x0")))
 					product_image = html_p.unescape(html_p.unescape(product_image_url.rsplit('/', 1)[-1]))
 					product_price = html_p.unescape(html_p.unescape(data[3]))
-					urlretrieve(product_image_url, '/var/www/html/online_store/assets/imgs/' + product_image)
+					try:
+						print("Getting image of product: ", product_image_url)
+						urlretrieve(product_image_url, '/var/www/html/online_store/assets/imgs/' + product_image)
+					except urllib.error.ContentTooShortError as e:
+						print("Image", product_image_url , "wasn't fully downloaded")
 					product_category = 1
 					for word in product_title.split(" "):
 						matched_categories = [x for x in result if len(word) >= 3 and re.match(r'' + re.escape(word[:-2]) + '', x['name'], re.IGNORECASE) and (len(word) >= (len(x['name'])/2))]
@@ -72,7 +95,8 @@ def visit_url(url, connection):
 					products_inserted+=cursor.rowcount
 					print("Products inserted: ", products_inserted)
 
-			matched = re.findall(r'<\s*a \s*href="([^"]*)"', lines)
+			matched = re.findall(r'<\s*a \s*href="([^"]*(?<!.jpg|.png|jpeg))"', lines, re.IGNORECASE)
+			response.close()
 			links = []
 			for link in matched:
 				if base_name in link:
@@ -81,16 +105,52 @@ def visit_url(url, connection):
 					links.append(base_url + link)
 			for link in links:
 				if link and link not in visited and validators.url(link):
+					#to_visit.append(link)
 					visit_url(link, connection)
-					
+		except OSError as e:
+			if isinstance(e, timeout):
+				print("Timeout occured retrying...")
+				visit_url(url, connection)
+			else:
+				print(e)
+		except pymysql.err.DataError as de:
+			print(de)
+		except pymysql.err.IntegrityError as ie:
+			print(ie)	
+		except pymysql.err.ProgrammingError as pe:
+			print(pe)
+		except pymysql.err.OperationalError as oe:
+			print(oe)
+		except pymysql.err.InternalError as ie:
+			print(ie)
+		except pymysql.err.Error as e:
+			print(e)
+		except UnicodeDecodeError as e:
+			print(e)
 		except Exception as e:
-			print('ijjiij',e)
+			print('Unknown error occured')
 
 
 if __name__ == "__main__":
-	main()
-
-
-#~ f = open('/var/www/html/online_store/assets/imgs/' + product_image, 'wb')
-#~ f.write(urlopen(product_image_url).read())
-#~ f.close()
+	try:
+		main()
+	except pymysql.err.OperationalError as oe:
+		print(oe)
+	except pymysql.err.InternalError as ie:
+		print(ie)
+	except pymysql.err.DatabaseError as de:
+		print(de)
+	except pymysql.err.InterfaceError as ie:
+		print(ie)
+	except pymysql.err.NotSupportedError as nse:
+		print(nse)
+	except pymysql.err.Error as e:
+		print(e)
+	except AttributeError as ae:
+		print(ae)
+	except TypeError as te:
+		print(ty)
+	except NameError as ne:
+		print(e)
+	except Exception as e:
+			print('Unknown error occured',e)
